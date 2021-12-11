271d777c937c1418ffba2c0b04a6ed9cccb52bb2
Leon <deadogone@gmail.com>
2021-12-11 15:06:06 +0800

    feat: 网络请求头 加入clash

--- src/main/java/me/leon/support/Ext.kt
+++ src/main/java/me/leon/support/Ext.kt
@@ -21,7 +21,7 @@ fun String.readFromNet() =
                     setRequestProperty(
                         "user-agent",
                         "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) " +
-                            "Chrome/86.0.4240.198 Safari/537.36"
+                            "Chrome/86.0.4240.198 Safari/537.36 Clash"
                     )
                 } as
                     HttpURLConnection)
