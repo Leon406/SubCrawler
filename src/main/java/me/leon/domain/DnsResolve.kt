c5a8bf89d39e8768d8cba95b355887bf10be8eef
Leon Lambert <deadogone@gmail.com>
2022-04-11 16:08:52 +0800

    ci: update detekt-analysis.yml

---
+++ src/main/java/me/leon/domain/DnsResolve.kt
@@ -0,0 +1,24 @@
+package me.leon.domain
+
+data class DnsResolve(
+    val Status: Int?,
+    val TC: Boolean?,
+    val RD: Boolean?,
+    val RA: Boolean?,
+    val AD: Boolean?,
+    val CD: Boolean?,
+    val Question: List<Question>?,
+    val Answer: List<Answer>?
+)
+
+data class Question(
+    val name: String?,
+    val type: Int?
+)
+
+data class Answer(
+    val name: String?,
+    val type: Int?,
+    val TTL: Int?,
+    val `data`: String?
+)
